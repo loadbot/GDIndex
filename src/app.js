ecce47772ebf1eb5456114dd5d8114d5945c5e85
T0M4S <83116688+loadbot@users.noreply.github.com>
2025-07-04 16:51:42 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 // Redesigned by telegram.dog/TheFirstSpeedster at https://www.npmjs.com/package/@googledrive/index which was written by someone else, credits are given on Source Page.
-// v2.3.8
+// v2.3.7
 // Initialize the page
 function init() {
     document.siteName = $('title').html();
